242628cd7e255e03edc05850db1ed6ab3ff380ef
00asgaroth00 <00asgaroth00@gmail.com>
2024-09-08 15:51:31 +0100

    borrow python scripts from JulioPDX/affinity

---
+++ convert.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+Script used to convert a bunch of SVG files to PNG
+"""
+
+from os import walk, mkdir
+import pyvips
+
+# sudo apt-get install libvips
+# pip install pyvips
+
+PATH = "svg/"
+
+
+def main():
+
+    """
+    All the action
+    """
+
+    for (root, dirs, files) in walk(PATH):
+        for f in files:
+            if ".svg" in f:
+                try:
+                    mkdir(path=f"{root}/png/")
+                except OSError:
+                    pass
+                convert_file = pyvips.Image.thumbnail(f"{root}/{f}", 52, height=52)
+                convert_file.write_to_file(f"{root}/png/{f.replace('svg', 'png')}")
+
+
+if __name__ == "__main__":
+    main()
