ef3c594a06338e8c6cffb2087508fce96a113c37
00asgaroth00 <00asgaroth00@gmail.com>
2024-09-08 16:05:20 +0100

    add comment for alternative methods for converting svg->png

--- convert.py
+++ convert.py
@@ -28,6 +28,9 @@ def main():
                     pass
                 convert_file = pyvips.Image.thumbnail(f"{root}/{f}", 52, height=52)
                 convert_file.write_to_file(f"{root}/png/{f.replace('svg', 'png')}")
+                # alternative method using cairosvg, taken from comment:
+                #    https://github.com/ecceman/affinity/issues/12#issuecomment-819598447
+                # cairosvg.svg2png(url=f"{root}/{f}", write_to=f"{root}/png/{f.replace('svg', 'png')}")
 
 
 if __name__ == "__main__":
